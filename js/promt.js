f3b5224f6b4c9c68b079ea25c73a8678ff6fe34a
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-11 07:56:04 +0600

    confirm utton complete

--- js/promt.js
+++ js/promt.js
@@ -2,3 +2,13 @@ const alertButton = () => {
     alert('Tomi vallo na ')
 }
 
+const confirmButton = () => {
+    const result = confirm('ami ki bari jabo ?');
+    // console.log(result);
+    if (result === true) {
+        alert('tomi bari jhole jaw')
+    }
+    else {
+        console.log('tomar aske kono choti nai');
+    }
+}
