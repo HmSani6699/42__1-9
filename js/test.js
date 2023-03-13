581e72b501a277676ccec7bd8bc60f51b53258c9
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-13 08:42:10 +0600

    get to product in localStorage

---
+++ js/test.js
@@ -0,0 +1,33 @@
+const addCard = () => {
+    const productFiled = document.getElementById('product-filed');
+    const productQuantity = document.getElementById('product-quantity');
+    const product = productFiled.value;
+    const quantity = productQuantity.value;
+
+    productFiled.value = ''
+    productQuantity.value = ''
+
+    // console.log(product, quantity);
+    displayProduct(product, quantity);
+}
+
+// Display product
+const displayProduct = (product, quantity) => {
+    const listContainer = document.getElementById('product-container');
+    const li = document.createElement('li');
+    li.innerHTML = `
+    ${product}: ${quantity}
+    `;
+    listContainer.appendChild(li)
+}
+
+
+const getProductToLocalStorage = () => {
+    let cart = {}
+    const cartString = localStorage.getItem('cart');
+    if (cartString) {
+        cart = JSON.parse(cartString)
+    }
+    return cart;
+}
+
